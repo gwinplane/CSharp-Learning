3a115727d12a13a0f19c5e865db1fea11e51a63f
gwinplane <maxmarinin@gmail.com>
2026-02-13 11:35:07 +0100

    Rechner

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class Rechner
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Taschenrechner ===");
+
+        Console.Write("Erste Zahl: ");
+
+        double zahl1;
+        
+        bool ok = double.TryParse(Console.ReadLine(), out zahl1);
+        if (ok)
+        {
+            Console.WriteLine($"Sie haben {zahl1} eingegeben");
+        }
+        else 
+        {
+            Console.WriteLine("Das ist nicht Zahl!");
+        }
+
+            Console.Write("Operation (+, -, *, /): ");
+        string operation = Console.ReadLine();
+
+        Console.Write("Zweite Zahl: ");
+
+        double zahl2;
+
+        bool ok1 = double.TryParse(Console.ReadLine(), out zahl2);
+        if (ok1)
+        {
+            Console.WriteLine($"Sie haben {zahl2} eingegeben");
+        }
+        else
+        {
+            Console.WriteLine("Das ist nicht Zahl!");
+        }
+
+        double ergebnis = 0;
+
+        if (operation == "+")
+        {
+            ergebnis = zahl1 + zahl2;
+        }
+        else if (operation == "-")
+        {
+            ergebnis = zahl1 - zahl2;
+        }
+        else if (operation == "*")
+        {
+            ergebnis = zahl1 * zahl2;
+        }
+        else if (operation == "/")
+        {
+            if (zahl2 != 0)
+            {
+                ergebnis = zahl1 / zahl2;
+            }
+            else
+            {
+                Console.WriteLine("Fehler: Division durch Null!");
+                return;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ungültige Operation!");
+            return;
+        }
+
+        Console.WriteLine($"Ergebnis: {ergebnis}");
+       
+    }
+    
+
+}
